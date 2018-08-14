73a8391d6a81b0a995a159409da7928ecff471df
sjh0824 <1204563557@qq.com>
2018-08-14 17:00:24 +0800

    位置授权

---
+++ src/pages/homePage/index.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="vist-userInfo">
+      <div class="sowingMap">
+        <swiper :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+            <swiper-item  v-for="(item,index) in imgUrls" :key="index">
+              <image :src="item" class="slide-image" style="width: 100%;height: 100%;"/>
+            </swiper-item>
+        </swiper>
+      </div>
+    <div class="containers">
+        <div class="leftNav">
+            <div v-for="(item,index) in dtasets" class="modeles" :key="index" :data-set="index" :class="{ hotModel: currentNum==index}" @click="changeNav(index)">
+                {{item.name}}
+
+              <div class="childModel" v-if="item.child.length>0" v-for="(items,ind) in item.child" :key="ind" :dataSet="ind"  :class="{ childHotModel: childCurNum==ind,childDis: currentNum==index}" @click="changeChildNav(ind)"  @touchstart.stop='changeChildNav(ind)'>
+                <p>
+                  {{items.name}}{{items.lines}}
+                </p>
+              </div>
+            </div>
+        </div>
+      <div class="rightCon">
+        <scroll-view scroll-y>
+          <div class="rightModel" @click="detailsPage('0')">
+            <img class="rightImg" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" alt="">
+            <p>惠普战 99 G1移动工作站</p>
+          </div>
+          <div class="rightModel">
+            <img class="rightImg" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" alt="">
+            <p>惠普战 99 G1移动工作站</p>
+          </div>
+          <div class="rightModel">
+            <img class="rightImg" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" alt="">
+            <p>惠普战 99 G1移动工作站</p>
+          </div>
+          <div class="rightModel">
+            <img class="rightImg" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" alt="">
+            <p>惠普战 99 G1移动工作站</p>
+          </div>
+          <div class="rightModel">
+            <img class="rightImg" src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg" alt="">
+            <p>惠普战 99 G1移动工作站</p>
+          </div>
+        </scroll-view>
+      </div>
+    </div>
+    <div class="maskModel" :class="{maskBlock: authorLoc== true}">
+      <div class="maskContainer">
+          <div class="maskText">“司马e”要获取你的地理位置，是否允许？</div>
+          <div class="maskBtn">
+            <button @click="cancle()">取消</button>
+            <button open-type="openSetting" @opensetting='handlerLocation' class="confirm">确定</button>
+          </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import QQMapWX from '../../../static/qqmap-wx-jssdk.min'
+  export default {
+    data() {
+      return {
+        currentNum:0,
+        childCurNum:0,
+        authorLoc: false,
+        arrImage: [],
+        checked: false,
+        qrcodeUrl: '',
+        ctxHeight: 650,
+        backImg: require('../../../static/img/shareBack.png'),
+        imgUrls: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: true,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000,
+        dtasets:[{name:'热门机型',child:[]},{name:'台式机',child:[{name:'Elite',lines:'精英系列'},{name:'Pro ',lines:'专家系列'}]},{name:'笔记本',child:[{name:'Elite',lines:'精英系列'},{name:'Pro ',lines:'专家系列'}]}]
+      }
+    },
+
+    onLoad() {
+      console.log(111111111)
+    },
+    onShow(){
+      var that = this;
+      wx.getSetting({
+        success: (res) => {
+          console.log(res);
+          console.log(res.authSetting['scope.userLocation']);
+          if(res.authSetting['scope.userLocation']){
+            that.location(that,QQMapWX);
+          }else{
+            if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {//非初始化进入该页面,且未授权
+              that.authorLoc = true;
+            } else if (res.authSetting['scope.userLocation'] == undefined) {//初始化进入
+              that.location(that,QQMapWX);
+            }
+          }
+
+        }
+      })
+    },
+    onShareAppMessage(res) {
+      var that = this;
+      if (res.from === 'button') {
+        // 来自页面内转发按钮
+        console.log(res.target)
+      }
+
+      var actId =that.$store.state.board.actId
+      var helpId = that.$store.state.board.myHelpId
+      var path = "/pages/activePower/main";
+
+      if (actId && helpId) {
+        path = path + "?actId=" + actId + "&helpId=" + helpId
+      } else {
+
+      }
+      return {
+        title: '分享活动',
+        path: path,
+        success: function () {
+//          wx.redirectTo({
+//            url:"/pages/showPages/main"
+//          })
+        }
+      }
+
+    },
+    methods: {
+      closePrev() {
+        this.checked = false;
+        wx.hideLoading()
+      },
+      changeNav(index){
+        this.childCurNum = 0;
+        this.currentNum = index
+      },
+      changeChildNav(ind){
+        this.childCurNum = ind
+      },
+      detailsPage(index){
+        wx.navigateTo({
+          url: '../detailPage/main?id='+index
+        })
+      },
+      cancle(){
+        this.authorLoc = false;
+      },
+      handlerLocation(){
+        this.authorLoc = false;
+      },
+      location(that,QQMapWX){
+        var qqmapsdk;
+        qqmapsdk = new QQMapWX({
+          key: 'OISBZ-SUKW6-LJ7SS-MXQHI-GC5FF-CQBGM'
+        });
+        wx.getLocation({
+          type: 'gcj02',
+          altitude: true,
+          success: (res) => {
+            console.log(res)
+            qqmapsdk.reverseGeocoder({
+              location: {
+                latitude: res.latitude,
+                longitude: res.longitude
+              },
+              success: (addressRes) => {
+                that.authorLoc = false;
+                console.log(addressRes);
+                that.$store.state.board.address = addressRes.result.address_component.province + '' + addressRes.result.address_component.district;
+                that.$store.state.board.location = addressRes.result.address_component.province;
+              }
+            })
+          }
+        })
+      }
+    },
+    async onPullDownRefresh() {
+      // 停止下拉刷新
+      wx.stopPullDownRefresh();
+    },
+
+    created() {
+    },
+    computed: {
+
+    },
+    mounted() {
+      var that = this;
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .vist-userInfo {
+    font-size: 22px;
+    width: 100%;
+    height: 100%;
+    .sowingMap{
+      width: 100%;
+      height: 98px;
+      swiper{
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .containers{
+      width: 100%;
+      height: calc(100% - 98px);
+      .leftNav{
+        width: 100px;
+        height: 100%;
+        float: left;
+        background-color: #F5F5F5;
+        .modeles{
+          width: 100%;
+          /*height: 50px;*/
+          line-height: 50px;
+          font-size: 14px;
+          color: #0096D6;
+          border-bottom: 1px solid #fff;
+          text-align: center;
+          .childModel{
+            width: 100%;
+            /*height: 50px;*/
+            line-height: 50px;
+            font-size: 12px;
+            color: #0096D6;
+            text-align: center;
+            border-bottom: 1px solid #fff;
+            display: none;
+            background-color: #F5F5F5;
+            p{
+              line-height: 50px;
+            }
+
+          }
+          .childHotModel{
+            color:#101010;
+            background-color: #fff;
+            border-left: 2px solid #0096D6;
+          }
+          div.childDis{
+            display: block;
+          }
+        }
+        .hotModel{
+          background-color: #0096D6;
+          color: #fff;
+          border-bottom: 0px;
+        }
+      }
+      .rightCon{
+        float: left;
+        width: calc(100% - 100px);
+        height: 100%;
+        /*background-color: #fff;*/
+        scroll-view{
+          height: 100% ;
+          .rightModel{
+            width: 256px;
+            height: 160px;
+            margin: 20px auto;
+            background-color: #F5F5F5;
+            text-align: center;
+            .rightImg{
+              width: 242px;
+              height: 100px;
+              margin: 0 auto;
+              margin-top: 20px;
+            }
+            p{
+              height: 23px;
+              margin-top: 5px;
+              font-size: 10px;
+              color: #5A5A5A;
+              line-height: 15px;
+            }
+          }
+        }
+
+      }
+    }
+    .maskModel{
+      width: 100%;
+      height: 100%;
+      display: none;
+      background-color: rgba(0,0,0,.7);
+      position: fixed;
+      left: 0px;
+      top:0px;
+      .maskContainer{
+        width: 80%;
+        height: 110px;
+        margin: 0 auto;
+        margin-top: 50%;
+        background-color: #fff;
+        .maskText{
+          padding: 20px;
+          padding-bottom: 10px;
+          font-size: 14px;
+          text-align: center;
+          color: #999;
+        }
+        .maskBtn{
+          button::after{
+            border:none;
+          }
+          button{
+            width: 50%;
+            float: left;
+            border-radius: 0px;
+            box-sizing: border-box;
+            border-top: 1px solid #ccc;
+            background-color: #fff;
+          }
+          .confirm{
+            color: #3CC51F;
+            border-left: 1px solid #ccc;
+          }
+        }
+      }
+    }
+    div.maskBlock{
+      display: block;
+    }
+  }
+</style>

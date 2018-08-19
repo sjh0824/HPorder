3e8ebd9e841a600460ae523d81a2f825317aab3e
sjh0824 <1204563557@qq.com>
2018-08-19 18:44:12 +0800

    登录

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,18 @@
       var that = this;
       console.log(option)
       this.$store.state.board.actId= '6';
+
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            utils.login(that)
+          }
+          else {
+            this.$store.state.board.authorizeFlag = false;
+          }
+        }
+      })
     },
     created() {
 
--- src/pages/homePage/index.vue
+++ src/pages/homePage/index.vue
@@ -128,13 +128,13 @@
               that.location(that,QQMapWX);
             }
           }
-           if (res.authSetting['scope.userInfo']) {
-             // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-             utils.login(that)
-           }
-           else {
-             this.$store.state.board.authorizeFlag = false;
-           }
+//           if (res.authSetting['scope.userInfo']) {
+//             // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+//             utils.login(that)
+//           }
+//           else {
+//             this.$store.state.board.authorizeFlag = false;
+//           }
         }
       })
     },
